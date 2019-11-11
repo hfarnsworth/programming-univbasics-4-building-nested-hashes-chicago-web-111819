a766b974772c8c225e4e633dd20400ecf889609d
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-11 07:29:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,6 +12,10 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
+  monopoly = {
+    railroads: pieces: Hash.new
+  }
+end
 end
 
 def monopoly_with_third_tier
