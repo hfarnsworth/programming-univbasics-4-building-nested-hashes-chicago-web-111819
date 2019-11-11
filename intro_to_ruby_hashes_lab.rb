704588ea8b182c2d89ba6902099e40d48bf3b0e7
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-11 07:52:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -60,7 +60,7 @@ def monopoly_with_fourth_tier
         :four_pieces_owned => 200
       },
       :names => {
-        :reading_railroad => Hash.new,
+        :reading_railroad => {"mortgage_value", "$" + 100},
         :pennsylvania_railroad => Hash.new,
         :b_and_o_railroad => Hash.new,
         :shortline_railroad => Hash.new
